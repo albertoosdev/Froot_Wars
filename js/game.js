5eb202add6788844b1eeebdeaa67132e197a081e
Alberto Ortega Santos <alberto_naval94@hotmail.com>
2018-01-29 18:22:27 +0100

    fixes

--- js/game.js
+++ js/game.js
@@ -1,14 +1,16 @@
-$(window).load(function(){
+"use strict";
+
+$(window).load(function () {
     game.init();
 });
 
 var levels = {
     //Nivel de datos
-    data:[
+    data: [
         {   //Primer nivel
-            foreground:'desert-foregrund',
+            foreground: 'desert-foregrund',
             background: 'clouds-background',
-            entities:[]
+            entities: []
         },
         {   // Segundo nivel
             foreground: 'desert-foreground',
@@ -17,87 +19,87 @@ var levels = {
         }
     ],
     //Inicializa la panttalla de seleccion de nivel
-    init:function(){
+    init: function () {
         var html = "";
-        for(var i=0; i< levels.data.length; i++){
+        var i;
+        for (i = 0; i < levels.data.length; i++) {
             var level = levels.data[i];
-            html += '<input type="button" value"'+(i+1)+'">';
-        };
+            html += '<input type="button" value"' + (i + 1) + '">';
+        }
         $('#levelselectedscreen').html(html);
         
         //Establece los controladores de eventos de click de boton para cargar el nivel
-        $('#levelselectedscreen inout').click(function(){
-            levels.load(this.value-1);
-            $$('#levelselectscreen').hide();
+        $('#levelselectedscreen inout').click(function () {
+            levels.load(this.value - 1);
+            $('#levelselectscreen').hide();
         });
     },
     
     //carga todos los datos e imagenes para un nivel especifico
-    load:function(number){
+    load: function (number) {
         
     }
-}
+};
 
 var loader = {
     loaded: true,
     loadedCount: 0, //Assets que han sido cargados antes
     totalCount: 0, //Numero total de assets que es necesario cargar
     
-    init:function(){
+    init: function () {
         //Comprueba el soporte para sonido
         var mp3Support, oggSupport;
         var audio = document.createElement('audio');
-        if(audio.canPlayType){
+        if (audio.canPlayType) {
             //Actualmente canPlayType() devuelve: "", "maybe" o "probably"
-            mp3Support = "" != audio.canPlayType('audio/mpeg');
-            oggSupport = "" != audio.canPlayType('audio/ogg; codecs="vorbis"');
-        }
-        else{
+            mp3Support = "" !== audio.canPlayType('audio/mpeg');
+            oggSupport = "" !== audio.canPlayType('audio/ogg; codecs="vorbis"');
+        } else {
             //La etiqueta de audio no es soportada
             mp3Support = false;
             oggSupport = false;
         }
         
         //Comprueba para ogg, mp3 y finalmnente fija soundFileExtn como undefined
-        loader.soundFileExtn = oggSupport?".ogg":mp3Support?".mp3":undefined;
+        loader.soundFileExtn = oggSupport ? ".ogg" : mp3Support ? ".mp3" : undefined;
     },
     
-    loadImage: function(url){
+    loadImage: function (url) {
         this.totalCount++;
         this.loaded = false;
         $('#loadingscreen').show();
         var audio = new Audio();
-        audio.src = url+loader.soundFileExtn;
-        audio.addEventListener("canplaythrough"), loader.itemLoaded, false);
+        audio.src = url + loader.soundFileExtn;
+        audio.addEventListener("canplaythrough", loader.itemLoaded, false);
         return audio;
     },
     
-    itemLoaded: function(){
+    itemLoaded: function () {
         loader.loadedCount++;
-        $('#loadingmessage').html('Loaded '+loader.loaderCount+' of '+loader.totalCount);
-        if(loader.loadedCount === loader.totalCount){
+        $('#loadingmessage').html('Loaded ' + loader.loaderCount + ' of ' + loader.totalCount);
+        if (loader.loadedCount === loader.totalCount) {
             //El loader ha cargado completamente
             loader.loaded = true;
             //Oculta la pantalla de carga
             $('#loadingscreen').hide();
             //Y llama al metodo loader.onLoad si este existe
-            if(loader.onload){
+            if (loader.onload) {
                 loader.onload();
                 
                 loader.onload = undefined;
             }
                 
-           }
+        }
     }
-}
+};
 
 var game = {
     //Comienza inicializando los objetos, precargando los assets y mostrando la pantalla de inicio
-    init:function(){
+    init: function () {
         //Inicializa objetos
         levels.init();
         loader.init();
-        mouse.init();
+        //mouse.init();
         
         //Oculta todas las capas del juego y muestra la pantalla de inicio
         $('.gamelayer').hide();
@@ -108,8 +110,8 @@ var game = {
         game.context = game.canvas.getContext('2d');
     },
     
-    showLevelScreen:function(){
+    showLevelScreen: function () {
         $('.gamelayer').hide();
         $('#levelselectscreen').show('slow');
-    },
-}
+    }
+};
